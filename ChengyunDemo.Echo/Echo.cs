64d6884e2d8246e6a9e498df2cbe095acd5b80dd
1194420729 <1194420729@qq.com>
2017-07-21 09:24:10 +0800

    build config

---
+++ ChengyunDemo.Echo/Echo.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text; 
+using System.Data; 
+using System.IO;
+using System.Web;
+using System.Net;
+using System.Configuration;
+using Jxc.Utility;
+
+namespace ChengyunDemo.Echo
+{
+    public class Echo : MarshalByRefObject
+    {
+        public Object GetModuleContent(string parameters)
+        {
+            ParameterHelper ph = new ParameterHelper(parameters);
+
+            DBHelper db = new DBHelper();
+
+            return new {  };
+
+        }
+         
+
+    }
+
+
+}
